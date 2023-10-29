4a66a988d8ec30ff197e6fd1d5498809ec505519
i200496-Khursheed <i200496@nu.edu.pk>
2023-10-29 15:10:27 +0500

    Added Block Verification for First & Last block

--- assignment01bca/assignment01bca.go
+++ assignment01bca/assignment01bca.go
@@ -4,59 +4,54 @@
 // Assignment: 2
 package assignment01bca
 
+// Importing Packages
 import (
-	"crypto/sha256"
-	"encoding/hex"
-	"fmt"
-	"strconv"
+	"crypto/sha256" // provides SHA256 hash encoding function
+	"encoding/hex"  // encodes & decodes hexadecimal strings
+	"fmt"           // printing output on console
+	"strconv"       // converts string to integer and vice versa
 )
 
-// MerkleNode represents a node in the Merkle Tree.
+// MerkleNode represents a node in the Merkle Tree. Leaf nodes show transaction, non-leaf nodes have hashes
 type MerkleNode struct {
-	Left  *MerkleNode
-	Right *MerkleNode
-	Hash  string
+	Left  *MerkleNode // left child of current merkle node
+	Right *MerkleNode // right child of current merkle node
+	Hash  string      // concatenates left + right child hash
 }
 
 // Block represents a block in the blockchain.
 type Block struct {
-	Transactions       []Transaction
-	PreviousHash       string
-	MerkleRoot         string
-	PreviousMerkleRoot string
-	Hash               string
+	Transactions       []Transaction // stores transaction (data + nonce) for multiple transaction in a single block
+	PreviousHash       string        // previous hash of previous block
+	MerkleRoot         string        // hash of merkle root of current block
+	PreviousMerkleRoot string        // previous merkle root hash to check for tampering of transaction
+	Hash               string        // Current block hash (current block merkle root hash + previous block hash)
 }
 
 // Transaction represents a transaction with data and nonce.
 type Transaction struct {
-	Data  string
-	Nonce int
+	Data  string // transaction data
+	Nonce int    // nonce for the transaction data
 }
 
-var Blockchain []Block
-var TransactionPool []Transaction
-var NumberOfTransactionsPerBlock = 2
-var BlockHashMin = "0000000000000000000000000000000000000000000000000000000000000000"
-var BlockHashMax = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+var Blockchain []Block                                                                // block slice
+var TransactionPool []Transaction                                                     // Transaction Pool to temporalily store Transactions
+var NumberOfTransactionsPerBlock = 2                                                  // total number of transactions to produce a new block
+var BlockHashMin = "0000000000000000000000000000000000000000000000000000000000000000" // lowest nonce target required
+var BlockHashMax = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" // highest nonce traget required
 
 // CreateHash calculates the hash of a block using the previous hash, Merkle root..
 func (b *Block) CreateHash() string {
-	data := b.PreviousHash + b.MerkleRoot
-	return CalculateHash(data)
+	data := b.PreviousHash + b.MerkleRoot // stores previous block hash and merkle root hash in data
+	return CalculateHash(data)            // calculates new hash of current block
 }
 
 // NewBlock creates a new block with the given transactions and adds it to the blockchain.
 func NewBlock(transactions []Transaction) {
+	// validation step to check if transaction is empty | Display Error if yes means length of transaction slice is 0
 	if len(transactions) == 0 {
 		fmt.Println("Error: No transactions provided")
 		return
-	}
-
-	var previousMerkleRoot string
-	if len(Blockchain) > 0 {
-		previousMerkleRoot = Blockchain[len(Blockchain)-1].MerkleRoot
-	} else {
-		previousMerkleRoot = ""
 	}
 
 	merkleRoot := BuildMerkleTree(transactions)
@@ -65,7 +60,7 @@ func NewBlock(transactions []Transaction) {
 		Transactions:       transactions,
 		PreviousHash:       getLastBlockHash(),
 		MerkleRoot:         merkleRoot,
-		PreviousMerkleRoot: previousMerkleRoot,
+		PreviousMerkleRoot: merkleRoot, // Store the current block's Merkle Root in both MerkleRoot and PreviousMerkleRoot
 	}
 
 	block.Hash = block.CreateHash()
@@ -86,11 +81,14 @@ func ChangeBlock(blockIndex int, transactionIndex int, newTransactionData string
 		return
 	}
 
+	// Store the current Merkle Root before changing the transaction
 	previousMerkleRoot := block.MerkleRoot
 
+	// Change the transaction data and nonce
 	block.Transactions[transactionIndex].Data = newTransactionData
 	block.Transactions[transactionIndex].Nonce = newNonce
 
+	// Recalculate the Merkle Root for the current block
 	block.MerkleRoot = BuildMerkleTree(block.Transactions)
 	block.Hash = block.CreateHash()
 
@@ -141,24 +139,41 @@ func DisplayTransactionsInBlock(index int) {
 
 // VerifyChain checks the integrity of the blockchain by comparing block hashes and Merkle Tree hashes.
 func VerifyChain() bool {
-	if len(Blockchain) < 2 {
-		fmt.Println("Less blocks to verify")
+	if len(Blockchain) < 1 {
+		fmt.Println("No blocks to verify")
 		return false
 	}
 
-	for i := 1; i < len(Blockchain); i++ {
+	// Start from index 0
+	for i := 0; i < len(Blockchain); i++ {
+		// For the first block, check if PreviousMerkleRoot Hash is equal to its current MerkleRoot Hash?
+		if i == 0 {
+			if Blockchain[i].PreviousMerkleRoot != Blockchain[i].MerkleRoot {
+				fmt.Println("Block 0 is compromised. Merkle Root Hash Mismatch!")
+				return false
+			}
+		} else {
+			// Verify the Merkle Tree
+			if Blockchain[i].PreviousMerkleRoot != Blockchain[i].MerkleRoot {
+				fmt.Println("Block", i, "is compromised. Merkle Tree mismatch.")
+				return false
+			}
 
-		// Verify the Merkle Tree
-		previousMerkleRoot := BuildMerkleTree(Blockchain[i-1].Transactions)
-		if Blockchain[i].PreviousMerkleRoot != previousMerkleRoot {
-			fmt.Println("Block", i, "is compromised. Merkle Tree mismatch.")
-			return false
+			if Blockchain[i].PreviousHash != Blockchain[i-1].Hash || !isBlockHashInRange(Blockchain[i].Hash) {
+				fmt.Println("Block", i, "is compromised. Block hash mismatch.")
+				return false
+			}
 		}
+	}
 
-		if Blockchain[i].PreviousHash != Blockchain[i-1].Hash || !isBlockHashInRange(Blockchain[i].Hash) {
-			fmt.Println("Block", i, "is compromised. Block hash mismatch.")
-			return false
-		}
+	// Verify the Merkle Tree of the last block
+	lastBlockIndex := len(Blockchain) - 1
+	previousMerkleRoot := BuildMerkleTree(Blockchain[lastBlockIndex].Transactions)
+	if Blockchain[lastBlockIndex].PreviousMerkleRoot != previousMerkleRoot {
+		fmt.Println("Last block is compromised. Merkle Tree mismatch.")
+		return false
+	} else {
+		fmt.Println("All TRX Valid.")
 	}
 
 	return true
@@ -183,36 +198,37 @@ func isBlockHashInRange(hash string) bool {
 	return hash >= BlockHashMin && hash <= BlockHashMax
 }
 
-// BuildMerkleTree builds the Merkle Tree and returns its root hash.
+// BuildMerkleTree builds the Merkle Tree and returns its root hash. Takes slice of transaction object
 func BuildMerkleTree(transactions []Transaction) string {
 	if len(transactions) == 0 {
 		return ""
 	}
 
-	var nodes []MerkleNode
+	var nodes []MerkleNode // slice of merkle nodes to hold leaf nodes of merkle tree
 
-	for _, trx := range transactions {
-		dataToHash := trx.Data + strconv.Itoa(trx.Nonce)
-		hash := CalculateHash(dataToHash)
-		nodes = append(nodes, MerkleNode{Hash: hash})
+	for _, trx := range transactions { // iterates over all transactions in each leaf node and assigns the current transaction to the trx vairable
+		dataToHash := trx.Data + strconv.Itoa(trx.Nonce) // put trx data and its nonce in dataToHash variable
+		hash := CalculateHash(dataToHash)                // dataToHash is hashed and put into hash variable of the concatenated dataToHash
+		nodes = append(nodes, MerkleNode{Hash: hash})    // a node is created into the merkle tree with the hash calculated
 	}
 
-	for len(nodes) > 1 {
-		var newLevel []MerkleNode
+	for len(nodes) > 1 { // iteration loop to run until more than 1 node i.e. until root node found
+		var newLevel []MerkleNode // newLelvel MerkleNode slice to hold new level of merkle tree nodes
 
-		for i := 0; i < len(nodes); i += 2 {
-			left := &nodes[i]
-			right := left
+		for i := 0; i < len(nodes); i += 2 { // increments of 2 to start i with left node and later check i+1 for right node to form pairs.
+			left := &nodes[i] // left is pointed towards the ith node
+			right := left     // sets right node to left node as well in-case of odd number of nodes
+
 			if i+1 < len(nodes) {
-				right = &nodes[i+1]
+				right = &nodes[i+1] // points right towards the next node if available within the same node slice
 			}
-			hash := CalculateHash(left.Hash + right.Hash)
-			node := MerkleNode{Left: left, Right: right, Hash: hash}
+			hash := CalculateHash(left.Hash + right.Hash)            // internal node hash caluclated by concatenating left and right nodes hash
+			node := MerkleNode{Left: left, Right: right, Hash: hash} // new node created with the left and right node set and concatenated hash calculated
 			newLevel = append(newLevel, node)
 		}
 
-		nodes = newLevel
+		nodes = newLevel // nodes updated to contain newLevel nodes created
 	}
 
-	return nodes[0].Hash
+	return nodes[0].Hash // the entire Merkle tree returned by its root hash.
 }
